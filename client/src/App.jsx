5440b9af145eeca115ffe9ab18ecee49ccf70a10
Brewerjg <grammerjasonl@gmail.com>
2023-12-19 22:55:37 -0600

    lazy load

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import Home from './views/home';
-import MenuView from './views/menuView'; 
-import TruckMap from './views/coffeeTruck';
-import AboutView from './views/aboutView';
-import LoginView from './views/loginView';
-import Dashboard from './views/dashboard';
-import RegisterView from './views/registerView';
-import UpdateView from './views/updateView';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Home = React.lazy(() => import('./views/home'));
+const MenuView = React.lazy(() => import('./views/menuView'));
+const TruckMap = React.lazy(() => import('./views/coffeeTruck'));
+const AboutView = React.lazy(() => import('./views/aboutView'));
+const LoginView = React.lazy(() => import('./views/loginView'));
+const Dashboard = React.lazy(() => import('./views/dashboard'));
+const RegisterView = React.lazy(() => import('./views/registerView'));
+const UpdateView = React.lazy(() => import('./views/updateView'));
+
 
 function App() {
-  
-
   return (
     <>
       <div>
         <BrowserRouter>
-        <Routes>
-            <Route element={<Home/>} path="/" />  
-            <Route element={<MenuView/>} path="/menu" />
-            <Route element={<TruckMap/>} path="/location" />
-            <Route element={<AboutView/>} path="/about" />
-            <Route element={<LoginView/>} path="/login" />
-            <Route element={<Dashboard/>} path="/dashboard" />
-            <Route element={<RegisterView/>} path="/register" />
-            <Route element={<UpdateView/>} path="/employee/:id"/>
-        </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route element={<Home />} path="/" />
+              <Route element={<MenuView />} path="/menu" />
+              <Route element={<TruckMap />} path="/location" />
+              <Route element={<AboutView />} path="/about" />
+              <Route element={<LoginView />} path="/login" />
+              <Route element={<Dashboard />} path="/dashboard" />
+              <Route element={<RegisterView />} path="/register" />
+              <Route element={<UpdateView />} path="/employee/:id" />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
